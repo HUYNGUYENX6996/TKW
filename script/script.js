dbecc35bdb7676b0d7fbea9fe2732a18c5c0dcc8
Oslamelon <91736051+ToAN7@users.noreply.github.com>
2024-08-31 09:18:39 +0700

    Show search on click

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 window.addEventListener("load", () => {
-    let search = this.document.getElementById("show-login");
-    let searchSh = this.document.querySelector(".search");
+    let search = document.getElementById("show-search");
+    let searchSh = document.querySelector(".search");
 
     search.addEventListener("click", () => {
         searchSh.classList.toggle("show");
