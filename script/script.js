34286083603a42f7b9364c792867e3d9fddc2b73
Oslamelon <91736051+ToAN7@users.noreply.github.com>
2024-09-01 09:35:41 +0700

    Add search function

--- script/script.js
+++ script/script.js
@@ -2,7 +2,35 @@ window.addEventListener("load", () => {
     let search = document.getElementById("show-search");
     let searchSh = document.querySelector(".search");
 
+    let searchBox = document.querySelector(".search-box");
+    let contentTitle = document.querySelectorAll(".content h3");
+    let contentSummary = document.querySelectorAll(".content summary")
+    
     search.addEventListener("click", () => {
         searchSh.classList.toggle("show");
+        for( let ctn of contentTitle) {
+            ctn.style.color = "black";
+        }
     });
+
+    searchBox.addEventListener("change", () => {
+        console.log(searchBox.value);
+        for( let ctn of contentTitle) {
+            console.log(10000);
+            ctn.style.color = "black";
+            if (ctn.innerHTML.match(searchBox.value) && searchBox.value !== '') {
+                console.log("access");
+                ctn.style.color = "lightblue";
+            }
+        }
+        for( let ctnSmr of contentSummary) {
+            ctnSmr.style.color = "black";
+            if (ctnSmr.innerHTML.match(searchBox.value) && searchBox.value !== '') {
+                console.log("access");
+                ctnSmr.style.color = "lightblue";
+            }
+        }
+        searchBox.value = '';
+    })
+
 });
