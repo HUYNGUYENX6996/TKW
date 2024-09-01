a6b7ab2bcb4d4b0f48ceb47628cf081beb20073a
Oslamelon <91736051+ToAN7@users.noreply.github.com>
2024-09-01 14:29:57 +0700

    CSSed with login

--- script/script.js
+++ script/script.js
@@ -2,17 +2,17 @@ window.addEventListener("load", () => {
     let search = document.getElementById("show-search");
     let searchSh = document.querySelector(".search");
 
-    let searchBox = document.querySelector(".search-box");
-    let contentTitle = document.querySelectorAll(".content h3");
-    let contentSummary = document.querySelectorAll(".content summary")
-    
     search.addEventListener("click", () => {
         searchSh.classList.toggle("show");
         for( let ctn of contentTitle) {
             ctn.style.color = "black";
         }
     });
-
+    
+    let searchBox = document.querySelector(".search-box");
+    let contentTitle = document.querySelectorAll(".content h3");
+    let contentSummary = document.querySelectorAll(".content summary")
+    
     searchBox.addEventListener("change", () => {
         console.log(searchBox.value);
         for( let ctn of contentTitle) {
