98d4e4d0ab21f6a75be9fe96bb77dcaed857539e
Oslamelon <91736051+ToAN7@users.noreply.github.com>
2024-09-01 18:47:56 +0700

    Add Huy code

---
+++ THE/THE.js
@@ -0,0 +1,18 @@
+window.onload = function(){
+    window.onscroll = function() {
+        if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+            document.getElementById("TopBtn").style.display = "block";
+        } else {
+            document.getElementById("TopBtn").style.display = "none";
+        }
+    };
+    // document.getElementById('TopBtn').addEventListener("click", function(){
+    //     document.body.scrollTop = 0;
+    //     document.documentElement.scrollTop = 0;
+    // });
+    document.getElementById('TopBtn').onclick = function() {
+        window.scrollTo({top: 0, behavior: 'smooth'}); // Cuộn lên đầu trang từ từ
+        
+    };
+    
+} 
